338a815b8122dd2297e7a7821cd4a99e5680517a
MauricioRivera21 <39868449+MauricioRivera21@users.noreply.github.com>
2021-12-16 02:49:20 -0500

    Update cinematica_diferencial.py

--- cinematica_diferencial.py
+++ cinematica_diferencial.py
@@ -1,12 +1,19 @@
 #!/usr/bin/env python
 #
- 
+
+#IN:
+#posición cartesiana deseada (xd)
+#pos articular inicial (q0)
+
+#OUT:
+#Movimiento hacia la posicion deseada
+
 from __future__ import print_function
 import rospy
 from sensor_msgs.msg import JointState
  
 from markers import *
-from Funciones import *
+from funciones import *
  
 from numpy import matrix, rank
  
@@ -15,8 +22,10 @@ rospy.init_node("testKineControlPose")
 print('starting motion ... ')
 # Publisher: publish to the joint_states topic
 pub = rospy.Publisher('joint_states', JointState, queue_size=1000)
-# Markers for the current and desired positions
+
+# Marker for the current position
 bmarker_current  = FrameMarker()
+# Marker for desired position
 bmarker_desired = FrameMarker(0.5)
  
 # Joint names
@@ -28,11 +37,12 @@ bmarker_desired = FrameMarker(0.5)
 alcanzado_max = [alcanzado_max_xy, alcanzado_max_z]
  
 # Desired pose
-ang = pi/3
-Rd = np.array([[0,1,0],[1,0,0],[0,0,-1]])
-qd = rot2quat(Rd)
+#ang = pi/3
+#Rd = np.array([[0,1,0],[1,0,0],[0,0,-1]])
+qd = rot2quat(Rd)        #conversion a quaternion
 # Find an xd that the robot can reach
-xd = np.array([0.4, 0.4, 0.4, qd[0], qd[1], qd[2], qd[3]])
+#xd = np.array([0.4, 0.4, 0.4, qd[0], qd[1], qd[2], qd[3]])
+
 if (xd[2] >= alcanzado_max[1]):
   xd[2]=alcanzado_max[1]
 if (xd[1] >= alcanzado_max[0]):
@@ -40,7 +50,6 @@ if (xd[1] >= alcanzado_max[0]):
 if (xd[0] >= alcanzado_max[0]):
   xd[0]=alcanzado_max[0]
  
-#xd  = np.array([0.5, 0.5, 0.6, np.cos(ang/2.0), 0, 0, np.sin(ang/2.0)])
 # Initial configuration
 q0  = np.array([0.0, -1.0, 1.7, -2.2, -1.6, 0.0])
  
@@ -81,31 +90,36 @@ while not rospy.is_shutdown():
   k = 1
   # Current time (needed for ROS)
   jstate.header.stamp = rospy.Time.now()
-  # Kinematic control law for the pose (complete here)
+  # Obtencion de Jacobiano analitico
   J = jacobian_pose(q, delta=0.0001)
+  
+  # Obtencion de valor de Quaternion (w,ex,ey,ez) des y actuales
   wd = qd[0]
   ed = qd[1:3]
   w = q[0]
   e = q[1:3]
  
-  #Qe = [wd*w + ed.T*e, -w*e + w*ed - np.cross(ed,e)]
+  #Error entre ambos Quaterniones (des y actual)
   Qe = np.hstack((wd*w + ed.T*e, -w*e + w*ed - np.cross(ed,e)))
  
-  #eo = [Qe[0]-1, Qe[1:3]]
+  #Error de orientacion:
   eo = np.hstack((Qe[0]-1, Qe[1:3]))   
  
-  #derror = [x-xd, eo]
+  #Error de posición y orientacion
   derror = np.hstack((xpos-xpos_des, eo))    
-  print(derror.shape)
+  print(derror)
   e_der = -k*derror   
  
   n=np.linalg.matrix_rank(J); m=J.ndim               #np.linalg.det(J)==0
+  
+  # Realizacion de la pseudoinversa correspondiente
   if (n<m):
     q_der = np.dot(np.dot(np,linalg.inv(J.T*J),J.T), e_der)      #pseudo inversa amortiguada
   else:
     q_der = np.dot(np.linalg.pinv(J), e_der)         
  
-  q = q + dt*q_der
+  #Valor de q actual después de realizar la integracion
+  q = q + dt*q_der    #q[k] = q[k-1] + t_delta*dq[k]
     
   # --------------------------------------------------
  
@@ -114,6 +128,7 @@ while not rospy.is_shutdown():
   T = fkine_ur5(q)
   x = TF2xyzquat(T)
  
+  #Salida del lazo
   min_err=0.1
   if(np.linalg.norm(xd-x0)<min_err):
     break
