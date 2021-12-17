5b7984e6ce6e7c38d0378be0cd74aa7f7ad2fb10
MauricioRivera21 <39868449+MauricioRivera21@users.noreply.github.com>
2021-12-17 01:02:39 -0500

    Update cinematica_diferencial.py

--- cinematica_diferencial.py
+++ cinematica_diferencial.py
@@ -18,54 +18,36 @@ from funciones import *
 from numpy import matrix, rank
  
 # Initialize the node
-rospy.init_node("testKineControlPose")
+rospy.init_node("testKineControlPosition")
 print('starting motion ... ')
 # Publisher: publish to the joint_states topic
-pub = rospy.Publisher('joint_states', JointState, queue_size=1000)
-
-# Marker for the current position
-bmarker_current  = FrameMarker()
-# Marker for desired position
-bmarker_desired = FrameMarker(0.5)
-
-# Archivos donde se almacenara los datos
-f_xpos = open("/tmp/xactual.dat", "w")
-f_xdes = open("/tmp/xdeseado.dat", "w")
-f_error = open("/tmp/xerror.dat", "w")
-
-
+pub = rospy.Publisher('joint_states', JointState, queue_size=10)
+# Files for the logs
+fxcurrent = open("/tmp/xcurrent.txt", "w")                
+fxdesired = open("/tmp/xdesired.txt", "w")
+fq = open("/tmp/q.txt", "w")
+ 
+# Markers for the current and desired positions
+bmarker_current  = BallMarker(color['RED'])
+bmarker_desired = BallMarker(color['GREEN'])
+ 
 # Joint names
 #jnames = ['shoulder_pan_joint', 'shoulder_lift_joint', 'elbow_joint',
 #          'wrist_1_joint', 'wrist_2_joint', 'wrist_3_joint']
  
-#alcanzado_max_z = (89.2+425+392+94.75)/100
-#alcanzado_max_xy = (425+392+94.75)/100
-alcanzado_max = [alcanzado_max_xy, alcanzado_max_z]
- 
-# Desired pose
-#ang = pi/3
-#Rd = np.array([[0,1,0],[1,0,0],[0,0,-1]])
-qd = rot2quat(Rd)        #conversion a quaternion
-# Find an xd that the robot can reach
-#xd = np.array([0.4, 0.4, 0.4, qd[0], qd[1], qd[2], qd[3]])
-
-if (xd[2] >= alcanzado_max[1]):
-  xd[2]=alcanzado_max[1]
-if (xd[1] >= alcanzado_max[0]):
-  xd[1]=alcanzado_max[0]
-if (xd[0] >= alcanzado_max[0]):
-  xd[0]=alcanzado_max[0]
- 
+# Desired position
+#xd = np.array([-0.8, -0.2, 0.0])
 # Initial configuration
-q0  = np.array([0.0, -1.0, 1.7, -2.2, -1.6, 0.0])
+#q0  = np.array([1.0, 0.0, 1.0, 3.0, 2.0, 0.5])
  
-# Resulting initial pose (end effector with respect to the base link)
+# Resulting initial position (end effector with respect to the base link)
 T = fkine_ur5(q0)
-x0 = TF2xyzquat(T)    #x0 = [x,y,z,w,ex,ey,ez]
+x0 = T[0:3,3]      #initial position(x,y,z)
  
-# Markers for the current and the desired pose
-bmarker_current.setPose(x0)
-bmarker_desired.setPose(xd)
+# Red marker shows the achieved position
+bmarker_current.xyz(x0)
+# Green marker shows the desired position
+bmarker_desired.xyz(xd)
  
 # Instance of the JointState message
 jstate = JointState()
@@ -76,86 +58,61 @@ jstate.name = jnames
 jstate.position = q0
  
 # Frequency (in Hz) and control period 
-freq = 200
+freq = 20
 dt = 1.0/freq
 rate = rospy.Rate(freq)
  
 # Initial joint configuration
 q = copy(q0)
-x = copy(x0)
-xpos = x[0:3]
-quat = x[3:7]
-xpos_des = xd[0:3]
-quat_des = xd[3:7]
- 
-# Initialize the derror vector (derivative of the error)
-derror = np.zeros(7)
 # Main loop
-#for i in range(1):
 while not rospy.is_shutdown():
-  k = 1
+  
+  k = 100
   # Current time (needed for ROS)
   jstate.header.stamp = rospy.Time.now()
-  # Obtencion de Jacobiano analitico
-  J = jacobian_pose(q, delta=0.0001)
+  # Kinematic control law for position (complete here)
+  # -----------------------------
+  #n=np.linalg.matrix_rank(J); m=J.ndim 
+  J = jacobian_ur5(q0, delta=0.0001)
+  e = x0 - xd
+  e_der = -k*e
   
-  # Obtencion de valor de Quaternion (w,ex,ey,ez) des y actuales
-  wd = qd[0]
-  ed = qd[1:3]
-  w = q[0]
-  e = q[1:3]
- 
-  #Error entre ambos Quaterniones (des y actual)
-  Qe = np.hstack((wd*w + ed.T*e, -w*e + w*ed - np.cross(ed,e)))
- 
-  #Error de orientacion:
-  eo = np.hstack((Qe[0]-1, Qe[1:3]))   
- 
-  #Error de posición y orientacion
-  derror = np.hstack((xpos-xpos_des, eo))    
-  print(derror)
-  e_der = -k*derror   
- 
-  error = xpos-xpos_des
-  # Almacenamiento de datos
-  fxact.write(str(t)+' '+str(x[0])+' '+str(x[1])+' '+str(x[2])+'\n')
-  fxdes.write(str(t)+' '+str(xdes[0])+' '+str(xdes[1])+' '+str(xdes[2])+'\n')
-  fqact.write(str(t)+' '+str(error[0])+' '+str(error[1])+' '+ str(error[2])+ '\n ')
- 
   n=np.linalg.matrix_rank(J); m=J.ndim               #np.linalg.det(J)==0
-  
-  # Realizacion de la pseudoinversa correspondiente
+  ##############################
   if (n<m):
-    q_der = np.dot(np.dot(np,linalg.inv(J.T*J),J.T), e_der)      #pseudo inversa amortiguada
+    q_der = np.dot(np.dot(np,linalg.inv(J.T*J),J.T), e_der) #pseudo inversa amortiguada
   else:
-    q_der = np.dot(np.linalg.pinv(J), e_der)         
+    q_der = np.dot(np.linalg.pinv(J), e_der)
  
-  #Valor de q actual después de realizar la integracion
-  q = q + dt*q_der    #q[k] = q[k-1] + t_delta*dq[k]
+  #q_der = np.dot(np.linalg.pinv(J), e_der)
+  q = q + dt*q_der
+  # -----------------------------
+  
+  T=fkine_ur5(q)   
+  x=T[0:3,3]
+  x0=x
+  # Log values                                                      
+  fxcurrent.write(str(x[0])+' '+str(x[1]) +' '+str(x[2])+'\n')
+  fxdesired.write(str(xd[0])+' '+str(xd[1])+' '+str(xd[2])+'\n')
+  fq.write(str(q[0])+" "+str(q[1])+" "+str(q[2])+" "+str(q[3])+" "+
+            str(q[4])+" "+str(q[5])+"\n")
     
-  # --------------------------------------------------
  
- 
-  # Current configuration trnaformation to current position
-  T = fkine_ur5(q)
-  x = TF2xyzquat(T)
- 
-  #Salida del lazo
-  min_err=0.1
-  if(np.linalg.norm(xd-x0)<min_err):
+  min_err=0.01
+  if(np.linalg.norm(e)<min_err):
     break
-  pass
+ 
  
   # Publish the message
   jstate.position = q
   pub.publish(jstate)
-  bmarker_desired.setPose(xd)
-  bmarker_current.setPose(x)
-    
+  bmarker_desired.xyz(xd)
+  bmarker_current.xyz(x)
  
   # Wait for the next iteration
   rate.sleep()
-
-f_error.close()
-f_xpos.close()
-f_xdes.close()
+ 
+print('ending motion ...')
+fxcurrent.close()
+fxdesired.close()
+fq.close()
